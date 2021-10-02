fa73d1168fd9ca425e6e6e4425e9e397c49b2c46
dancorrea86 <danielmacielcorrea@gmail.com>
2021-10-02 08:26:56 -0300

    Adicionado botão sair

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from ../.users import models as usermodel
 
-# Create your models here.
+from PIL import Image
+
+class Book(models.Model):
+    name = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+    description = models.TextField()
+    book_pic = models.ImageField(upload_to='books', default="book/book_profile.png", blank=True, null=True)
+    # readers = models.ManyToManyField(usermodel.User)
+
+    def __str__(self):
+        return self.name
